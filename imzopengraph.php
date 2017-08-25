fcaf1a966d206b4c8a594e5628a68c44a70a36c3
ausnichts <kazzmion@gmail.com>
2017-08-25 16:27:36 +0900

    v 1.0.0d2

--- imzopengraph.php
+++ imzopengraph.php
@@ -13,12 +13,25 @@ class plgSystemImzOpenGraph extends JPlugin {
 	function onContentPrepareForm($form, $data) {
 
 		$app = JFactory::getApplication();
+		
+		if(!$app->isAdmin()) {
+			return;
+		}
+		
 		$option = $app->input->get('option');
- 
-		if($app->isAdmin() && $option === 'com_content') {
 
+ 
+		if($option === 'com_content') {
+
+			$xml = simplexml_load_file( __DIR__ . '/forms/content.xml');
+			$xml->fields->fieldset->field[0]['default'] = $this->params->get('og_content_default');
+			$xml->fields->fieldset->field[6]['default'] = $this->params->get('tw_content_default');
+			$xml->fields->fieldset->field[7]['default'] = $this->params->get('tw_card');
+			$form->load($xml);
+
+		} else if($option === 'com_menus') {
 			JForm::addFormPath(__DIR__ . '/forms');
-			$form->loadFile('content', false); 
+			$form->loadFile('menu', false);
 		}
 
 	}
@@ -34,13 +47,13 @@ class plgSystemImzOpenGraph extends JPlugin {
 
 		$option = $app->input->get('option', '');
 		$view = $app->input->get('view', '');
-		$layout = $app->input->get('layout', '');
 		$scope = $option . '.' . $view;
 //		var_dump($scope);
 
 		$isHome = false;
-		$menu = $app->getMenu();
-		if ($menu->getActive() == $menu->getDefault()) {
+		$menus = $app->getMenu();
+		$thisMenu = $menus->getActive();
+		if ($thisMenu === $menus->getDefault()) {
 			$isHome = true;
 		}
 
@@ -65,12 +78,53 @@ class plgSystemImzOpenGraph extends JPlugin {
 			if (!empty($this->params->get('tw_site'))) $this->setTwData('twitter:site', $this->params->get('tw_site'));
 			if (!empty($this->params->get('tw_creator'))) $this->setTwData('twitter:creator', $this->params->get('tw_creator'));
 			
-		} elseif($scope === 'com_content.category' && $layout == 'blog') {
-			// カテゴリブログ
-		} elseif($scope === 'com_content.featured') {
-			// 注目記事
+		} elseif($scope !== 'com_content.article') {
+			// ホームでもなく記事でもないページはメニューで設定
+			if ($thisMenu->params->get('og_active')) {
+
+				$this->type = $thisMenu->params->get('og_type');
+				$this->setOgData('og:type', $this->type);
+				$tmp = !empty($thisMenu->params->get('og_title')) ? $thisMenu->params->get('og_title') : $doc->getTitle();
+				$this->setOgData('og:title', $tmp);
+
+				$tmp = "";
+				if(!empty($thisMenu->params->get('og_image'))) {
+					$tmp = JURI::base() . $thisMenu->params->get('og_image');
+				}else{
+					// 個別設定はしたがイメージ指定がない場合、ページ内から拾えるか？
+				}
+				if(empty($tmp) && !empty($this->params->get('imzopengraphimage'))) {
+					$tmp = JURI::base() . $this->params->get('imzopengraphimage');
+				}
+				$this->setOgData('og:image', $tmp);
+
+				$this->setOgData('og:url', JURI::getInstance()->toString());
+
+				$tmp= "";
+				if(!empty($thisMenu->params->get('og_description'))) {
+					$tmp = $thisMenu->params->get('og_description');
+				} else {
+					// 個別設定はしたがdescription指定がない場合、ページ内から拾えるか？
+				}
+				if(empty($tmp)) {
+					$tmp = !empty($this->params->get('imzopengraphdesc')) ? $this->params->get('imzopengraphdesc') : $globalDesc;
+				}
+				$this->setOgData('og:description', $tmp);
+
+				$tmp = !empty($this->params->get('imzopengraphsitename')) ? $this->params->get('imzopengraphsitename') : $globalSitename;
+				$this->setOgData('og:site_name', $tmp);
+			}
+
+			if ($thisMenu->params->get('tw_active')) {
+				$this->setTwData('twitter:card', $thisMenu->params->get('tw_card'));
+				$tmp = !empty($thisMenu->params->get('tw_site')) ? $thisMenu->params->get('tw_site') : $this->params->get('tw_site');
+				$this->setTwData('twitter:site', $tmp);
+				$tmp = !empty($thisMenu->params->get('tw_creator')) ? $thisMenu->params->get('tw_creator') : $this->params->get('tw_creator');
+				$this->setTwData('twitter:creator', $tmp);
+			}
 		}
-		$this->setOgData('fb:app_id', $this->params->get('imzopengraphfbappid'));
+		
+		if (!empty($this->ogData)) $this->setOgData('fb:app_id', $this->params->get('imzopengraphfbappid'));
 
 		$doc->addCustomTag('<!-- IMUZA Open Graph Plugin -->');
 		foreach($this->twData as $name => $value) {
@@ -109,54 +163,54 @@ class plgSystemImzOpenGraph extends JPlugin {
 			return;
 		}
 		
-		$tmp = !empty($article->params->get('og_type')) ? $article->params->get('og_type') : 'article';
-		$this->setOgData('og:type', $tmp);
-		$this->type = $tmp;
+		if ($article->params->get('og_active') || $this->params->get('og_content_default')) {
 
-		$tmp = !empty($article->params->get('og_title')) ? $article->params->get('og_title') : $article->title;
-		$this->setOgData('og:title', $tmp);
+			$tmp = !empty($article->params->get('og_type')) ? $article->params->get('og_type') : 'article';
+			$this->setOgData('og:type', $tmp);
+			$this->type = $tmp;
 
-		$tmp = "";
-		if(!empty($article->params->get('og_image'))) {
-			$tmp = JURI::base() . $article->params->get('og_image');
-		}else{
-			$images = json_decode($article->images);
-			if(!empty($images->image_intro) || !empty($images->image_fulltext)) {
-				$tmp = !empty($images->image_intro) ? JURI::base() . $images->image_intro : JURI::base() . $images->image_fulltext;
+			$tmp = !empty($article->params->get('og_title')) ? $article->params->get('og_title') : $article->title;
+			$this->setOgData('og:title', $tmp);
+
+			$tmp = "";
+			if(!empty($article->params->get('og_image'))) {
+				$tmp = JURI::base() . $article->params->get('og_image');
 			}else{
-				$fulltext = $article->introtext . $article->fulltext;
-				$flag = preg_match('/<img[^src]*src\s*=\s*[\"\']([^\"\']*).*>/i', $fulltext, $matches);
-				if($flag){
-					$tmp = JURI::base() . $matches[1];
+				$images = json_decode($article->images);
+				if(!empty($images->image_intro) || !empty($images->image_fulltext)) {
+					$tmp = !empty($images->image_intro) ? JURI::base() . $images->image_intro : JURI::base() . $images->image_fulltext;
+				}else{
+					$fulltext = $article->introtext . $article->fulltext;
+					$flag = preg_match('/<img[^src]*src\s*=\s*[\"\']([^\"\']*).*>/i', $fulltext, $matches);
+					if($flag){
+						$tmp = JURI::base() . $matches[1];
+					}
 				}
 			}
-		}
-		if(empty($tmp) && !empty($this->params->get('imzopengraphimage'))) {
-			$tmp = JURI::base() . $this->params->get('imzopengraphimage');
-		}
-		$this->setOgData('og:image', $tmp);
+			if(empty($tmp) && !empty($this->params->get('imzopengraphimage'))) {
+				$tmp = JURI::base() . $this->params->get('imzopengraphimage');
+			}
+			$this->setOgData('og:image', $tmp);
 
-		$this->setOgData('og:url', JURI::getInstance()->toString());
-		if(!empty($article->params->get('og_description'))) {
-			$this->setOgData('og:description', $article->params->get('og_description'));
-		} else {
-			$tmp = strip_tags($article->introtext);
-			$tmp = str_replace(array(PHP_EOL, "\t"), array(' ', ' '), $tmp);
-			$tmp = mb_strimwidth( $tmp, 0, 240, "...", "UTF-8" );
-			$this->setOgData('og:description', $tmp);
+			$this->setOgData('og:url', JURI::getInstance()->toString());
+			if(!empty($article->params->get('og_description'))) {
+				$this->setOgData('og:description', $article->params->get('og_description'));
+			} else {
+				$tmp = strip_tags($article->introtext);
+				$tmp = str_replace(array(PHP_EOL, "\t"), array(' ', ' '), $tmp);
+				$tmp = mb_strimwidth( $tmp, 0, 240, "...", "UTF-8" );
+				$this->setOgData('og:description', $tmp);
+			}
 		}
 
-		$tmp = !empty($article->params->get('tw_card')) ? $article->params->get('tw_card') : $this->params->get('tw_card');
-		$this->setTwData('twitter:card', $tmp);
-		if (!empty($article->params->get('tw_site')) || !empty($this->params->get('tw_site'))) {
+		if ($article->params->get('tw_active') || $this->params->get('tw_content_default')) {
+			$tmp = !empty($article->params->get('tw_card')) ? $article->params->get('tw_card') : $this->params->get('tw_card');
+			$this->setTwData('twitter:card', $tmp);
 			$tmp = !empty($article->params->get('tw_site')) ? $article->params->get('tw_site') : $this->params->get('tw_site');
 			$this->setTwData('twitter:site', $tmp);
-		}
-		if (!empty($article->params->get('tw_creator')) || !empty($this->params->get('tw_creator'))) {
 			$tmp = !empty($article->params->get('tw_creator')) ? $article->params->get('tw_creator') : $this->params->get('tw_creator');
 			$this->setTwData('twitter:creator', $tmp);
 		}
-
 	}
 
 	private function setOgData($name, $value) {
